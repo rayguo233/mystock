de9f90071754fd98df35e3ae4738537a368555e5
Ray Guo <siruiguo@outlook.com>
2021-08-29 14:54:11 +0800

    added portfolio models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ModelWithTime(models.Model):
+    update_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Transaction(ModelWithTime):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    ticker = models.CharField(max_length=20)
+    num_shares = models.IntegerField(max_length=20)
+    currency = models.CharField(max_length=20)
+    total_cost = models.DecimalField(max_digits=20, decimal_places=6)
+    transaction_fee = models.DecimalField(max_digits=10, decimal_places=6)
+
+class Portfolio(ModelWithTime):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    ticker = models.CharField(max_length=20)
+    curr_num_shares = models.IntegerField(max_length=20)
